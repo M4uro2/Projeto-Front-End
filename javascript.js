36014ff1661b20c688fc62b33231b2d973ac6bee
M4uro2 <mauro.neto@academico.ifpb.edu.br>
2025-03-03 18:44:05 -0300

    Adicionando a função para renderizar as terefas

--- javascript.js
+++ javascript.js
@@ -19,6 +19,20 @@ function inicializarAplicacao() {
                 alert(erro.message);
             }
         };
+         // Função para renderizar as tarefas
+         const renderizarTarefas = () => {
+            listaTarefas.innerHTML = tarefasFiltradas.map(tarefa => `
+            <li>
+                <strong>${tarefa.title}</strong>
+                <p>Usuário: ${tarefa.userId}</p>
+                <p>${tarefa.completed ? 'Concluída' : 'Pendente'}</p>
+                <p>${tarefa.description || ''}</p>
+                <button class="btn-editar" onclick="editarTarefa(${tarefa.id})">Editar</button>
+                <button class="btn-excluir" onclick="excluirTarefa(${tarefa.id})">Excluir</button>
+                ${!tarefa.completed ? `<button class="btn-concluir" onclick="concluirTarefa(${tarefa.id})">Concluir</button>` : ''}
+            </li>
+        `).join('');
+        };
     });
 }
 inicializarAplicacao();
